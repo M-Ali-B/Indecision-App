86599ae52f3c9140a9b587fbee8aa488453b31dd
Ali Baba <alibabatorjan001@gmail.com>
2018-10-03 21:43:04 +0500

    build-it-visually  and counter.js  are converted to the react components

--- src/playground/build-it-visually.js
+++ src/playground/build-it-visually.js
@@ -1,47 +1,105 @@
-const app = {
-title:'Build It',
-heading:'Visibility toggle',
-message:'YEs here are your details',
-visibilty:false
+class VisibilityToggle extends React.Component {
+constructor(props){
+super(props);
 
-};
-
-const toggleVisibilty= ()=>{
-
-    app.visibilty = !app.visibilty;
-     
-   renderApp();
-
-};
+this.toggleVisibility = this.toggleVisibility.bind(this);
 
 
-const renderApp = ()=>{
-    const template = (
+    this.state = {
+    visibility: false,
+        title: 'Build It',
+      heading:'Visibility toggle',
+      message:'YEs here are your details'
+    };
+            }
 
+            toggleVisibility(){
+
+                this.setState((prevState) => {
+
+                    return {
+
+                        visibility : !prevState.visibility
+                    };
+                });
+            }
+
+render(){
+    return(
         <div>
-        
-            <title>{app.title}</title>
-            <h1>{app.heading}</h1>
-<button onClick ={toggleVisibilty}>
-{app.visibilty ?'Hide details':'Show details'}
-</button>
-{
-                
-                    app.visibilty &&
-                        (
-                            <p>{app.message}</p>
-                        )
-                
+            <title>{this.state.title}</title>
+             <h1>{this.state.heading}</h1>
+ <button onClick ={this.toggleVisibility}>
+ { 
+ 
+     this.state.visibility ?'Hide details':'Show details'}
+ </button>
+ {
 
-}
-</div>
+                    this.state.visibility &&
+                         (
+                             <p>{this.state.message}</p>
+                         )
 
+
+ }
+
+
+        </div>
 
     );
-    ReactDOM.render(template, appRoot);
-
-};
+}
 
 
-let appRoot = document.getElementById('app');
-renderApp();
+}
+
+ReactDOM.render(<VisibilityToggle/>,document.getElementById('app'));
+
+
+// const app = {
+// title:'Build It',
+// heading:'Visibility toggle',
+// message:'YEs here are your details',
+// visibilty:false
+
+// };
+
+// const toggleVisibilty= ()=>{
+
+//     app.visibilty = !app.visibilty;
+     
+//    renderApp();
+
+// };
+
+
+// const renderApp = ()=>{
+//     const template = (
+
+//         <div>
+        
+//             <title>{app.title}</title>
+//             <h1>{app.heading}</h1>
+// <button onClick ={toggleVisibilty}>
+// {app.visibilty ?'Hide details':'Show details'}
+// </button>
+// {
+                
+//                     app.visibilty &&
+//                         (
+//                             <p>{app.message}</p>
+//                         )
+                
+
+// }
+// </div>
+
+
+//     );
+//     ReactDOM.render(template, appRoot);
+
+// };
+
+
+// let appRoot = document.getElementById('app');
+// renderApp();
