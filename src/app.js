0c0572e6ec14fd02a863c570ece560f6a4ae3177
Ali Baba <alibabatorjan001@gmail.com>
2018-10-19 23:26:14 +0500

    react,react-dom,babel with webpack and .babelrc(file) added

--- src/app.js
+++ src/app.js
@@ -1,13 +1,19 @@
-//import './utils.js';
- import {square} from './utils.js';
+// //import './utils.js';
+//  import {square} from './utils.js';
 
 
-console.log('app is running!');
-console.log(square(4));
+// console.log('app is running!');
+// console.log(square(4));
 
-import isSenior, { isAdult, canDrink } from './person.js';
-console.log(isAdult(22));
-console.log(canDrink(23));
-console.log(isSenior(55));
+// import isSenior, { isAdult, canDrink } from './person.js';
+// console.log(isAdult(22));
+// console.log(canDrink(23));
+// console.log(isSenior(55));
 
+import React from  "react";
+import ReactDOM from "react-dom";
+
+const template = <p>Hello brother</p>;
+
+ReactDOM.render(template,document.getElementById('app'));
 
